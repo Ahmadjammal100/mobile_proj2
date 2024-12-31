c0252d02e83ad536104bc740a3d59034f058769b
Ahmadjammal100 <silverlenovo2023@gmail.com>
2024-12-31 17:17:02 +0200

    This is my 2nd mobile app project

---
+++ flutter_application_1/lib/searchsubjects.php
@@ -0,0 +1,24 @@
+ <!-- These headers are put by me here so I can overpass the api restrictions of awardspace -->
+<?php
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+?>
+<?php
+$host ='fdb1028.awardspace.net';
+$user ='4558776_books';
+$password ='booklibrary2025@#';
+$dbname = '4558776_books';
+
+$con = new mysqli($host, $user, $password, $dbname);
+$name = $_GET['name'];
+$sql = "SELECT id, name, description, credits FROM subjects WHERE name LIKE '%$name%'";
+$result = mysqli_query($con, $sql);
+
+$subjects = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $subjects[] = $row;
+}
+
+echo json_encode($subjects);
+?>
